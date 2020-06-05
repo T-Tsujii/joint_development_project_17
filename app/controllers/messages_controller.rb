4be73f232e9a960da9793c8e6510ddce7a83118a
yuukii125 <y.kipro125@gmail.com>
2020-06-05 22:38:14 +0900

    new,create実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,19 @@
 class MessagesController < ApplicationController
+
+
+  #新規投稿
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.create(message_params)
+  end
+
+  
+  private
+  def message_params
+    params.permit(:message).require(:title, :content)
+  end
+  
 end
