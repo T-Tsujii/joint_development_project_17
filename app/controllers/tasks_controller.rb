49e3605309b24aa79cda32d900e74edf99b4dcca
Shunsuke Noguchi <bakutensanba89@yahoo.ne.jp>
2020-06-08 15:41:53 +0900

    一覧表示機能の追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,14 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
   def new
     @task= Task.new
+    
   end
   def create
     Task.create(task_params)
+    redirect_to action: :index
   end
 
   private
